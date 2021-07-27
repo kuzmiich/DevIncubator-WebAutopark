27440ccdd2f854ae3ba0349bbfd2fdfe848ae197
иван кузьмич <ivankuzmic210@gmail.com>
2021-07-27 19:14:11 +0300

    Added entity : order, orderDetail

---
+++ WebAutopark.Core/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAutopark.Core.Entities.Base;
+
+namespace WebAutopark.Core.Entities
+{
+    public class Order : Entity
+    {
+        public int OrderId { get; set; }
+
+        public int VehicleId { get; set; }
+    }
+}
---
+++ WebAutopark.Core/Entities/OrderDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAutopark.Core.Entities.Base;
+
+namespace WebAutopark.Core.Entities
+{
+    public class OrderDetail : Entity
+    {
+        public int OrderDetailId { get; set; }
+        
+        public Detail Detail { get; set; }
+
+        public int DetailId { get; set; }
+
+        public int DetailAmount { get; set; }
+
+    }
+}
