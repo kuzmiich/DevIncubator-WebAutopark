afbbf6a58eba294c131eeb1b04a900e2fd7209f6
Ivan <ivan.kuzmiich@gmail.com>
2021-09-03 15:47:15 +0300

    Edited Vehicle/Index page(take from dev-4-week) Updated VehicleViewModel

--- WebAutopark.BusinessLogic/Models/VehicleViewModel.cs
+++ WebAutopark.BusinessLogic/Models/VehicleViewModel.cs
@@ -13,17 +13,24 @@ namespace WebAutopark.BusinessLogic.Models
         #region Vehicle Property
 
         public int VehicleId { get; set; }
+        [Required] 
         public int VehicleTypeId { get; set; }
+
         public VehicleTypeViewModel VehicleType { get; set; }
         [MaxLength(30)] 
         public string ModelName { get; set; }
         [MaxLength(20)] 
         public string RegistrationNumber { get; set; }
         public int ManufactureYear { get; set; }
+        [Range(0, int.MaxValue)]
         public int Weight { get; set; }
+        [Range(0, int.MaxValue)]
         public int Mileage { get; set; }
+        [Range(1, 8)]
         public ColorType Color { get; set; }
+        [Range(0, double.MaxValue)]
         public double EngineConsumption { get; set; }
+        [Range(0, double.MaxValue)]
         public double TankCapacity { get; set; }
         
         public virtual double GetCalcTaxPerMonth => (VehicleType is not null) ? Math.Round(Weight * WeightCoefficient + VehicleType.TaxCoefficient * TaxCoefficient + ShiftForTax, 2) : 0d;
